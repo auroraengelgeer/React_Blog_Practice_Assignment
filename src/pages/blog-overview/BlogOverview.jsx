dcfc742503010b8755a73bba7a545fd76aaf30ae
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-10-15 17:55:31 +0200

    displayed data from data.json in BlogOverview page

--- src/pages/blog-overview/BlogOverview.jsx
+++ src/pages/blog-overview/BlogOverview.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import data from "../../constants/data.json"
+import {Link} from "react-router-dom";
 
 function BlogOverview() {
     return (
         <main className="blog-overview">
-            <h1>Dit is de blog overview pagina</h1>
+            <h1>Er zijn {data.length} blogposts</h1>
+
+            <ul>
+                {data.map((post) => (
+                    <li key={post.id}>
+                        <Link to={`/posts/${post.id}`}>
+                            {post.title} ({post.author})
+                        </Link>
+                        <p>{post.comments} reacties en {post.shares} keer gedeeld</p>
+                    </li>
+                ))}
+            </ul>
+
         </main>
     )
 }
