0ffb83462cf9fd2b3b149eec1530881f29f6588f
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-10-15 19:17:23 +0200

    implemented data into BlogpostDetail page

--- src/pages/BlogpostDetail/BlogpostDetail.jsx
+++ src/pages/BlogpostDetail/BlogpostDetail.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import data from '../../constants/data.json'
 
 function BlogpostDetail() {
 
-    const { id } = useParams();
+    const {id} = useParams();
+    const post = data.find(p => p.id === parseInt(id));
+
+    if (!post) {
+        return <p>Blogpost niet gevonden</p>;
+    }
 
     return (
         <main className="blog-detail">
-            <h1>Dit is blog {id}</h1>
+            <h1>{post.title}</h1>
+            <p>Geschreven door {post.author} op {post.created}</p>
+            <p>{post.content}</p>
+            <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
         </main>
-    )
+
+
+)
 }
 
 export default BlogpostDetail;
