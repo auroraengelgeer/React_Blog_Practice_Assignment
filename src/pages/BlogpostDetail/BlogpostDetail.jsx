ae24287c38e55af1bbd4fe1ab52273f8db854884
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-10-15 19:37:22 +0200

    made dateConvertor helper function and implemented it in BlogpostDetail

--- src/pages/BlogpostDetail/BlogpostDetail.jsx
+++ src/pages/BlogpostDetail/BlogpostDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import data from '../../constants/data.json'
+import dateConvertor from "../../helpers/date-convertor/dateConvertor.jsx";
 
 function BlogpostDetail() {
 
@@ -14,7 +15,7 @@ function BlogpostDetail() {
     return (
         <main className="blog-detail">
             <h1>{post.title}</h1>
-            <p>Geschreven door {post.author} op {post.created}</p>
+            <p>Geschreven door {post.author} op {dateConvertor(post.created)}</p>
             <p>{post.content}</p>
             <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
         </main>
